650215e54dabaa1a39342fea2f69492d8f7cfda3
mabalekasihle@gmail.com <mabalekasihle@gmail.com>
2018-04-26 01:30:54 +0200

    Initial commit of project files

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,148 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+/**
+ * Generated class for the HomePage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  courses = []
+  lessons = []
 
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.courses = courses;
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad HomePage');
   }
 
 }
+
+const courses = [
+  {
+    title: "Soft Skills in Programming",
+    number: 1,
+    icon: 'bicycle',
+    weeks : 3,
+    lessons : [
+      {
+        title: "Introduction and Team Building",
+        number: 1
+      }, 
+      {
+        title: "Agile Scrum Training",
+        number: 2
+      }, 
+      {
+        title: "Introduction to HTML, CSS and JavaScript",
+        number: 3
+      }, 
+    ]
+  },
+  {
+    title: "Building Blocks of the Web",
+    number: 2,
+    icon: 'logo-html5',
+    weeks : 4,
+    lessons : [
+      {
+        title: "Dive Deeper into HTML",
+        number: 1
+      }, 
+      {
+        title: "Styling HTML Pages",
+        number: 2
+      }, 
+      {
+        title: "JavaScript : An Intro to Angular",
+        number: 3
+      }, 
+      {
+        title: "Angular",
+        number: 4
+      }, 
+    ]
+  },
+  {
+    title: "Ionic Development for Beginners",
+    number: 3,
+    weeks : 3,
+    icon: 'ionic',
+    lessons : [
+      {
+        title: "Building Layouts",
+        number: 1
+      }, 
+      {
+        title: "Making an app interactive",
+        number: 2
+      }, 
+      {
+        title: "Work with the Ionic Framework and Control Flow",
+        number: 3
+      }, 
+    ]
+  },
+  {
+    title: "Multi Screen Apps",
+    number: 4,
+    icon: 'logo-angular',
+    weeks : 4,
+    lessons : [
+      {
+        title: "Pages and Navigation",
+        number: 1
+      }, 
+      {
+        title: "Data, Loops, and Custom Classes",
+        number: 2
+      }, 
+      {
+        title: "Images and Visual Polish",
+        number: 3
+      }, 
+      {
+        title: "Native API Libraries",
+        number: 4
+      }, 
+    ]
+  },
+  {
+    title: "Networking",
+    number: 5,
+    icon: 'wifi',
+    weeks : 4,
+    lessons : [
+      {
+        title: "JSON Parsing",
+        number: 1
+      }, 
+      {
+        title: "HTTP Networking",
+        number: 2
+      }
+    ]
+  },
+  {
+    title: "The Cloud",
+    number: 6,
+    icon: 'cloud',
+    weeks : 4,
+    lessons : [
+      {
+        title: "Firebase",
+        number: 1
+      } 
+    ]
+  }
+]
---
+++ src/pages/news/news.ts
@@ -0,0 +1,88 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the NewsPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-news',
+  templateUrl: 'news.html',
+})
+export class NewsPage {
+
+  news = []
+  curriculums = []
+  type : string = "general"
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.news = news.reverse();
+    this.curriculums = curriculum_news.reverse();
+    console.log(this.curriculums);
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad NewsPage');
+  }
+
+}
+
+const news = [
+  {
+    title: "CodeTribe Academy graduation",
+    time: "March, the 2nd",
+    article : "So, another chapter closes - no pun intended. March saw a lot of our CodeTribe students graduating and moving on to their next journey."
+  },
+  {
+    title: "CodeTribe Digital-Gen graduation",
+    time: "March, the 31st",
+    article : "This chapter of CodeTribe saw us do something we have never done before..."
+  },
+  {
+    title: "CodeTribe applications open",
+    time: "February, the 24th",
+    article : "CodeTribe Academy applications 2018/2019 now open. Please ensure your loved ones read the requirements but also, we'd like to know who you are..."
+  },
+  {
+    title: "CodeTribe Alex closed",
+    time: "March, the 17th",
+    article : "It's always sad when you endevour to create something , to plant a seed only to have it later uprooted. This was the case when we had to shut down..."
+  },
+  {
+    title: "Slots still available",
+    time: "A day ago",
+    article : "Our Soweto and Tembisa chapters are still looking for people to be part of the programme. Should you know anyone that is deserving of this opportunity then by all means..."
+  }
+]
+
+const curriculum_news = [
+  {
+    title: "New and improved curriculum",
+    time: "January, the 14th",
+    article : "Every now and again there comes a time when..."
+  },
+  {
+    title: "New lesson plans",
+    time: "February, the 11th",
+    article : "CodeTribe is predominantly lesson based and what this..."
+  },
+  {
+    title: "New practice sets",
+    time: "February, the 12th",
+    article : "We have also added a couple of practice..."
+  },
+  {
+    title: "Curriculum Review",
+    time: "March, the 6th",
+    article : "To ensure a consistent standard throughout"
+  },
+  {
+    title: "CodeTribe opening",
+    time: "A day ago",
+    article : "It's important that you get yourself familiarized with this curriculum. This will go..."
+  }
+]
